2f45ab5855a46b86ceb902ca8b1f171b4b6fdb81
Ernest Shefer <shef-er@ya.ru>
2025-06-01 11:08:08 +0500

    v0.1.0

---
+++ closer.go
@@ -0,0 +1,71 @@
+package closer
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+// NewCloser returns Closer and executes goroutine that awaits for ctx.Done() then run close(...) method
+func NewCloser(ctx context.Context, errFunc ErrFunc, timeout time.Duration) *Closer {
+	c := &Closer{
+		done:       make(chan struct{}, 1),
+		closeFuncs: make([]CloseFunc, 0),
+		errFunc:    errFunc,
+	}
+
+	go func() {
+		<-ctx.Done()
+
+		closeCtx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		c.close(closeCtx)
+	}()
+
+	return c
+}
+
+// Closer struct
+type Closer struct {
+	mu         sync.Mutex
+	done       chan struct{}
+	closeFuncs []CloseFunc
+	errFunc    ErrFunc
+}
+
+// CloseFunc is called when closer receives ctx.Done()
+type CloseFunc func(context.Context) error
+
+// ErrFunc is called to handle error
+type ErrFunc func(error)
+
+// Add closer func
+func (c *Closer) Add(f CloseFunc) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.closeFuncs = append(c.closeFuncs, f)
+}
+
+func (c *Closer) close(ctx context.Context) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	go func() {
+		for _, f := range c.closeFuncs {
+			if err := f(ctx); err != nil {
+				c.errFunc(err)
+			}
+		}
+
+		c.done <- struct{}{}
+	}()
+
+	select {
+	case <-c.done:
+		break
+	case <-ctx.Done():
+		c.errFunc(ctx.Err())
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shef-er/closer
+
+go 1.24.3
